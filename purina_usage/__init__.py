5d1392b9fdce4108927f767bb9d0c91f0e17fe9a
Joseph Van Drunen <josephvandrunen@gmail.com>
2023-09-19 09:00:12 -0400

    fixup

--- purina_usage/__init__.py
+++ purina_usage/__init__.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 from dagster import (
     Definitions,
     ScheduleDefinition,
@@ -5,14 +8,44 @@ from dagster import (
     fs_io_manager,
     load_assets_from_package_module,
 )
-from dagster._utils import file_relative_path
-from dagster_dbt import dbt_cli_resource, load_assets_from_dbt_project
+from dagster_dbt import DbtCliResource, dbt_assets
 from dagster_snowflake_pandas import snowflake_pandas_io_manager
 
 from purina_usage.assets import raw_data, usage
 
-DBT_PROJECT_DIR = file_relative_path(__file__, "../dbt_project")
-DBT_PROFILES_DIR = file_relative_path(__file__, "../dbt_project/config")
+from dagster import OpExecutionContext
+from .utils import dagster_insights
+
+dbt_project_dir = Path(__file__).joinpath("..", "..", "dbt_project").resolve()
+dbt = DbtCliResource(
+    project_dir=os.fspath(dbt_project_dir),
+    profiles_dir=os.fspath(dbt_project_dir.joinpath("config")),
+    target="staging",
+)
+
+# If DAGSTER_DBT_PARSE_PROJECT_ON_LOAD is set, a manifest will be created at run time.
+# Otherwise, we expect a manifest to be present in the project's target directory.
+if os.getenv("DAGSTER_DBT_PARSE_PROJECT_ON_LOAD"):
+    dbt_parse_invocation = dbt.cli(["parse"]).wait()
+    dbt_manifest_path = dbt_parse_invocation.target_path.joinpath("manifest.json")
+else:
+    dbt_manifest_path = dbt_project_dir.joinpath("target", "manifest.json")
+
+
+# class CustomDagsterDbtTranslator(DagsterDbtTranslator):
+#     @classmethod
+#     def get_group_name(cls, _unused):
+#         return "test_group"
+
+
+@dbt_assets(manifest=dbt_manifest_path)
+def dbt_snowflake_assets(context: OpExecutionContext, dbt: DbtCliResource):
+    dbt_cli_invocation = dbt.cli(["build"], context=context)
+    yield from dbt_cli_invocation.stream()
+
+    run_results = dbt_cli_invocation.get_artifact("run_results.json")
+    manifest = dbt_cli_invocation.get_artifact("manifest.json")
+    dagster_insights.store_dbt_adapter_metrics(context, manifest, run_results)
 
 
 def partition_key_to_dbt_vars(partition_key):
@@ -20,17 +53,17 @@ def partition_key_to_dbt_vars(partition_key):
 
 
 # all assets live in the default dbt_schema
-dbt_assets = load_assets_from_dbt_project(
-    DBT_PROJECT_DIR,
-    DBT_PROFILES_DIR,
-    # prefix the output assets based on the database they live in plus the name of the schema
-    key_prefix=["snowflake", "dbt_schema"],
-    # prefix the source assets based on just the database
-    # (dagster populates the source schema information automatically)
-    source_key_prefix=["snowflake"],
-    # partitions_def=DailyPartitionsDefinition(start_date="2023-07-01"),
-    # partition_key_to_vars_fn=partition_key_to_dbt_vars,
-)
+# dbt_assets = load_assets_from_dbt_project(
+#     DBT_PROJECT_DIR,
+#     DBT_PROFILES_DIR,
+#     # prefix the output assets based on the database they live in plus the name of the schema
+#     key_prefix=["snowflake", "dbt_schema"],
+#     # prefix the source assets based on just the database
+#     # (dagster populates the source schema information automatically)
+#     source_key_prefix=["snowflake"],
+#     # partitions_def=DailyPartitionsDefinition(start_date="2023-07-01"),
+#     # partition_key_to_vars_fn=partition_key_to_dbt_vars,
+# )
 
 raw_data_assets = load_assets_from_package_module(
     raw_data,
@@ -50,20 +83,20 @@ usage_assets = load_assets_from_package_module(
 raw_job = define_asset_job(
     "raw_job", selection=["snowflake/raw_data/users", "snowflake/raw_data/orders"]
 )
-dbt_job = define_asset_job(
-    "dbt_job",
-    selection=[
-        "snowflake/dbt_schema/company_perf",
-        "snowflake/dbt_schema/company_stats",
-        "snowflake/dbt_schema/daily_order_summary",
-        "snowflake/dbt_schema/order_stats",
-        "snowflake/dbt_schema/orders_augmented",
-        "snowflake/dbt_schema/orders_cleaned",
-        "snowflake/dbt_schema/sku_stats",
-        "snowflake/dbt_schema/top_users",
-        "snowflake/dbt_schema/users_cleaned",
-    ],
-)
+# dbt_job = define_asset_job(
+#     "dbt_job",
+#     selection=[
+#         "snowflake/dbt_schema/company_perf",
+#         "snowflake/dbt_schema/company_stats",
+#         "snowflake/dbt_schema/daily_order_summary",
+#         "snowflake/dbt_schema/order_stats",
+#         "snowflake/dbt_schema/orders_augmented",
+#         "snowflake/dbt_schema/orders_cleaned",
+#         "snowflake/dbt_schema/sku_stats",
+#         "snowflake/dbt_schema/top_users",
+#         "snowflake/dbt_schema/users_cleaned",
+#     ],
+# )
 
 resources = {
     # this io_manager allows us to load dbt models as pandas dataframes
@@ -79,21 +112,15 @@ resources = {
     # this io_manager is responsible for storing/loading our pickled machine learning model
     "model_io_manager": fs_io_manager,
     # this resource is used to execute dbt cli commands
-    "dbt": dbt_cli_resource.configured(
-        {
-            "project_dir": DBT_PROJECT_DIR,
-            "profiles_dir": DBT_PROFILES_DIR,
-            "target": "staging",
-        }
-    ),
+    "dbt": dbt,
 }
 
 
 defs = Definitions(
-    assets=[*dbt_assets, *raw_data_assets, *usage_assets],
+    assets=[dbt_snowflake_assets, *raw_data_assets, *usage_assets],
     resources=resources,
     schedules=[
         ScheduleDefinition(job=raw_job, cron_schedule="@daily"),
-        ScheduleDefinition(job=dbt_job, cron_schedule="*/5 * * * *"),
+        # ScheduleDefinition(job=dbt_job, cron_schedule="*/5 * * * *"),
     ],
 )
---
+++ purina_usage/utils/dagster_insights.py
@@ -0,0 +1,157 @@
+import os
+import requests
+import time
+import snowflake.connector
+
+
+from dagster import OpExecutionContext
+
+URL = "http://127.0.0.1:3000/test/staging/graphql/"
+
+PUT_CLOUD_METRICS_MUTATION = """
+mutation CreateOrUpdateExtenalMetrics(
+  $metrics: [ExternalMetricInputs]!) {
+  createOrUpdateExternalMetrics(metrics: $metrics) {
+    __typename
+    ... on CreateOrUpdateExternalMetricsSuccess {
+      status
+      __typename
+    }
+    ...MetricsFailedFragment
+    ...UnauthorizedErrorFragment
+    ...PythonErrorFragment
+  }
+}
+fragment PythonErrorFragment on PythonError {
+  __typename
+  message
+  stack
+  causes {
+    message
+    stack
+    __typename
+  }
+}
+fragment MetricsFailedFragment on CreateOrUpdateExternalMetricsFailed {
+  __typename
+  message
+}
+fragment UnauthorizedErrorFragment on UnauthorizedError {
+  __typename
+  message
+}
+"""
+
+
+def get_snowflake_usage(query_id, database):
+    con = snowflake.connector.connect(
+        user=os.getenv("DAGSTER_INSIGHTS_SNOWFLAKE_USER"),
+        password=os.getenv("DAGSTER_INSIGHTS_SNOWFLAKE_PASSWORD"),
+        account=os.getenv("DAGSTER_INSIGHTS_SNOWFLAKE_ACCOUNT"),
+        warehouse=os.getenv("DAGSTER_INSIGHTS_SNOWFLAKE_WAREHOUSE"),
+        database="SNOWFLAKE",
+        schema="ACCOUNT_USAGE",
+    )
+
+    # Create a cursor object
+    cur = con.cursor()
+
+    # Write a SQL query
+    query = f"""
+SELECT
+    QUERY_ID,
+    TOTAL_ELAPSED_TIME,
+    BYTES_SCANNED,
+    BYTES_WRITTEN,
+    COMPILATION_TIME,
+    EXECUTION_TIME,
+    CREDITS_USED_CLOUD_SERVICES
+FROM QUERY_HISTORY
+WHERE DATABASE_NAME = '{database}'
+AND QUERY_ID = '{query_id}'
+"""
+
+    while True:
+
+        # Execute the query
+        cur.execute(query)
+
+        # Fetch all the rows
+        rows = cur.fetchall()
+        if len(rows) > 0:
+            break
+        time.sleep(30)
+    return rows
+
+
+def store_dbt_adapter_metrics(
+    context: OpExecutionContext,
+    manifest,
+    run_results,
+):
+    # store the manifest and run results somewhere
+    # for now, we'll just print them
+    if (
+        manifest["metadata"]["dbt_schema_version"]
+        != "https://schemas.getdbt.com/dbt/manifest/v9.json"
+    ):
+        context.log.warn(
+            f"unexpected dbt schema version: {manifest['metadata']['dbt_schema_version']}, required: https://schemas.getdbt.com/dbt/manifest/v9.json"
+        )
+        return
+    if (
+        run_results["metadata"]["dbt_schema_version"]
+        != "https://schemas.getdbt.com/dbt/run-results/v4.json"
+    ):
+        context.log.warn(
+            f"unexpected dbt schema version: {manifest['metadata']['dbt_schema_version']}, required: https://schemas.getdbt.com/dbt/run-results/v4.json"
+        )
+        return
+    # store the manifest and run results somewhere
+    metric_graphql_input = {}
+    assetMetricDefinitions = []
+    for result in run_results["results"]:
+        node = manifest["nodes"][result["unique_id"]]
+        metric_values = []
+        for adapter_response_key in result["adapter_response"]:
+            if adapter_response_key in ["_message", "code"]:
+                continue
+            # yield (adapter_response_key, result['adapter_response'][adapter_response_key])
+            if adapter_response_key == "query_id" and "database" in node:
+                context.log.info(
+                    get_snowflake_usage(
+                        result["adapter_response"][adapter_response_key], node["database"]
+                    )
+                )
+            context.log.info(
+                f"metric: {node['name']}.{adapter_response_key}: {result['adapter_response'][adapter_response_key]}"
+            )
+            metric_values.append(
+                {
+                    "metricValue": result["adapter_response"][adapter_response_key],
+                    "metricName": adapter_response_key,
+                }
+            )
+        assetMetricDefinitions.append(
+            {
+                "assetKey": node["name"],
+                "metricValues": metric_values,
+            }
+        )
+    metric_graphql_input = {
+        "runId": context.run_id,
+        "stepKey": context.get_step_execution_context().step.key,
+        "codeLocationName": context.dagster_run.external_job_origin.location_name,
+        "repositoryName": (
+            context.dagster_run.external_job_origin.external_repository_origin.repository_name
+        ),
+        "assetMetricDefinitions": assetMetricDefinitions,
+    }
+
+    variables = {"metrics": metric_graphql_input}
+    response = requests.post(
+        URL, json={"query": PUT_CLOUD_METRICS_MUTATION, "variables": variables}, timeout=300
+    )
+    response.raise_for_status()
+    json = response.json()
+    return json
